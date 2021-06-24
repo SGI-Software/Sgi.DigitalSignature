59d6a72078f5b4b0a5dbe5b1c31093446992bebe
Nicolas Soares Note <nicolasoliveirasoares@gmail.com>
2021-06-24 09:38:16 -0300

    Correções na URL de download parte 2

--- Sgi.DigitalSignature/AssinarHelper.cs
+++ Sgi.DigitalSignature/AssinarHelper.cs
@@ -7,7 +7,7 @@ namespace Sgi.DigitalSignature
         public const string FORM_KEY_PDF= "pdf";
         public const string FORM_KEY_CERTIFICADO_PFX = "certificadoPfx";
         public const string FORM_KEY_JSON = "json";
-        private const string ROTA_API_NO_SERVIDOR = "assinaturadigital/api/assinar/";
+        private const string ROTA_API_NO_SERVIDOR = "assinaturadigital/";
 
         public static string BASE_URL(HttpRequest request, bool ehProducao) 
             => ehProducao ? $"{request.Scheme}://{request.Host}/{ROTA_API_NO_SERVIDOR}" : $"{request.Scheme}://{request.Host}/" ;
